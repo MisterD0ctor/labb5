75f39bdd1fd3631a94814e73b51c5d554136ed7a
Zeb Mühlbach <Zeb Mhlbach@DESKTOP-718IHOE>
2024-02-27 15:40:39 +0100

    thing

--- labb5/src/labb5/GUI.java
+++ labb5/src/labb5/GUI.java
@@ -1,14 +1,28 @@
 package labb5;
 
+import java.awt.Canvas;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame{
+
+	JPanel canvas = new JPanel();
+	GridBagConstraints layout = new GridBagConstraints();
+	
+	//Container myPane = canvas.getContentPane();
+    //myPane.setLayout(new GridBagLayout());
+	
 	public static void main(String args[]){
-	       JFrame frame = new JFrame("My First GUI");
+	       JFrame frame = new JFrame();
 	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	       frame.setSize(300,300);
 	       JButton button = new JButton("Press");
+	       FlowLayout flow_layout = new FlowLayout();
 	       frame.getContentPane().add(button); // Adds Button to content pane of frame
 	       frame.setVisible(true);
 	       
