9d463f27c7b7fe742018594e639a8d8fd98cdbe4
kaspe <kaspe@130.240.109.204>
2024-03-11 15:05:59 +0100

    getters och setters

--- labb5/src/labb5/Situation.java
+++ labb5/src/labb5/Situation.java
@@ -8,10 +8,10 @@ import javax.swing.JLabel;
  * @author Zeb Muhlbach
  */
 public class Situation {
-	State state = State.Input1;
-	JLabel display;
-	BinOpButton binaryOperator;
-	int leftOperand;
+	private State state = State.Input1;
+	private JLabel display;
+	private BinOpButton binaryOperator;
+	private int leftOperand;
 	
 	/**
 	 * Creates a situation with a display
@@ -20,4 +20,45 @@ public class Situation {
 	public Situation(JLabel display) {
 		this.display = display;
 	}
+	
+	/**
+	 * Gets the 
+	 * @return an instance of State
+	 */
+	public State getState() {
+		return state;
+	}
+	
+	/**
+	 * Gets the display
+	 * @return an instance of JLabel
+	 */
+	public JLabel getDisplay() {
+		return display;
+	}
+	
+	/**
+	 * Gets the binary operator
+	 * @return an instance of BinOpButton
+	 */
+	public BinOpButton getBinaryOperator() {
+		return binaryOperator;
+	}
+	
+	/**
+	 * Sets the binary operator
+	 * @param binaryOperator an instance of BinOpButton
+	 */
+	public void setBinaryOperator(BinOpButton binaryOperator) {
+		this.binaryOperator = binaryOperator;
+	}
+	
+	public int getLeftOperand() {
+		return leftOperand;
+	}
+	
+	public void setLeftOperand(int leftOperand) {
+		this.leftOperand = leftOperand;
+	}
+	
 }
